3eb30e1c379437ed12d908c393e64e35321dce4e
Martin Tomsik <martin@internetmate.uk>
2020-09-12 00:46:12 +0100

    Added TodoItem component to separate logic.

---
+++ src/TodoItem.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default class TodoApp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        console.log(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        e.persist();
+        this.setState(() => ({
+            items: itemsModified(this.props.state.items)
+        }), () => {
+            console.log(this.props.state.items);
+        });
+
+        let itemsModified = (items) => {
+            items[e.target.dataset.index].done = e.target.checked ? true : false;
+            return items;
+        };
+
+    }
+
+    render() {
+        return (
+            <li key={this.props.item.id}>
+                <label>
+                    <input type="checkbox" value={this.props.item.id} defaultChecked={this.props.item.done} onChange={this.handleChange} data-index={this.props.index} />
+                    <span className={this.props.item.done ? "done" : ""}>{this.props.item.text}</span>
+                </label>
+            </li>
+        );
+    }
+
+};

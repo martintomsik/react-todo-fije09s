2c00a30bf8065db6904947546eb2fc559a61e12c
martintomsik <martin.tomsik@gmail.com>
2020-09-11 19:28:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-todo-fije09s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-todo-fije09s)
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import TodoApp from "./App";
+
+ReactDOM.render(<TodoApp />, document.getElementById("root"));

f0911418fdcf8fa292852807a382872b9b681f22
martintomsik <martin.tomsik@gmail.com>
2020-10-29 17:52:03 +0000

    Class name corrected to TodoIem.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,36 +1,44 @@
-import React from 'react';
+import React from "react";
 
-export default class TodoApp extends React.Component {
+export default class TodoItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.handleChange = this.handleChange.bind(this);
-    }
+  handleChange(e) {
+    e.persist();
+    this.setState(
+      () => ({
+        items: itemsModified(this.props.state.items)
+      }),
+      () => {
+        console.log(this.props.state.items);
+      }
+    );
 
-    handleChange(e) {
-        e.persist();
-        this.setState(() => ({
-            items: itemsModified(this.props.state.items)
-        }), () => {
-            console.log(this.props.state.items);
-        });
+    let itemsModified = items => {
+      items[e.target.dataset.index].done = e.target.checked ? true : false;
+      return items;
+    };
+  }
 
-        let itemsModified = (items) => {
-            items[e.target.dataset.index].done = e.target.checked ? true : false;
-            return items;
-        };
-
-    }
-
-    render() {
-        return (
-            <li key={this.props.item.id}>
-                <label>
-                    <input type="checkbox" value={this.props.item.id} defaultChecked={this.props.item.done} onChange={this.handleChange} data-index={this.props.index} />
-                    <span className={this.props.item.done ? "done" : ""}>{this.props.item.text}</span>
-                </label>
-            </li>
-        );
-    }
-
-};
+  render() {
+    return (
+      <li key={this.props.item.id}>
+        <label>
+          <input
+            type="checkbox"
+            value={this.props.item.id}
+            defaultChecked={this.props.item.done}
+            onChange={this.handleChange}
+            data-index={this.props.index}
+          />
+          <span className={this.props.item.done ? "done" : ""}>
+            {this.props.item.text}
+          </span>
+        </label>
+      </li>
+    );
+  }
+}
